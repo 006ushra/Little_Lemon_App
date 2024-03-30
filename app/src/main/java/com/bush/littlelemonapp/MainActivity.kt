98af0eb270fd8b9241930a0e124a184230265a23
Bushra <bshra.m@sybertechnology.com>
2024-03-31 01:50:14 +0200

    fun: added rest api dependency to get menu items from external source

--- app/src/main/java/com/bush/littlelemonapp/MainActivity.kt
+++ app/src/main/java/com/bush/littlelemonapp/MainActivity.kt
@@ -2,14 +2,30 @@ package com.bush.littlelemonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.lifecycle.lifecycleScope
 import com.bush.littlelemonapp.uiComponents.LowerPanel
 import com.bush.littlelemonapp.uiComponents.TopAppBar
 import com.bush.littlelemonapp.uiComponents.UpperPanel
 import com.bush.littlelemonapp.uiTheme.LittleLemonTheme
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,5 +37,16 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        lifecycleScope.launch {
+            getMenuItems()
+        }
+    }
+
+    private suspend fun getMenuItems() {
+        val response = httpClient.get("https://raw.githubusercontent.com/006ushra/Little_Lemon_App/master/home_screen_menu.json") {
+            header("Authorization", "TOKEN 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")
+        }
+            .body<String>()
+        Log.d("RESPONSE", response)
     }
 }
